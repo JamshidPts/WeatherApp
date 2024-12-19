4ec000cde021c95e94ff963088e61156e4b7bcf3
JamshidPts <jamshidpts14@gmail.com>
2024-12-19 11:36:32 +0500

    I configured responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='container'>
-    <div className='flex justify-between bg-[#074799] text-white p-[15px] '>
-      <a href="">Home</a>
-      <a href="">Search city</a>
-      <a href="">About us</a>
-    </div>
+      <div className='text-white font-sans font-bold p-[15px] w-full phone:bg-[orange] phone:flex phone:justify-between tablet:bg-[red] laptop:bg-[blue] desktop:bg-[black]'>
+        <a href="">Home</a>
+        <a href="">Search city</a>
+        <a href="">About us</a>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'phone': { max: '480px' },                // Максимум 480px (мобильные)
+        'tablet': { min: '481px', max: '768px' },// 481px до 768px
+        'laptop': { min: '769px', max: '1024px' },// 769px до 1024px
+        'desktop': { min: '1025px' },     // 4K экраны
+      },
+    },
   },
   plugins: [],
 }
