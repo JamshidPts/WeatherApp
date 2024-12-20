eef0d5d5d2a90838715704cfde6d70e4d311cbde
JamshidPts <jamshidpts14@gmail.com>
2024-12-20 18:52:25 +0500

    i configured ContextProvider to exchange props and Routes

--- src/router/WeatherRouter.jsx
+++ src/router/WeatherRouter.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import MainPage from '../pages/MainPage'
+import ContextProvider from '../context/Context'
 
 function WeatherRouter() {
   return (
-    <>
-    </>
+    <ContextProvider>
+      <Routes>
+        <Route path='/' element={<MainPage />}/>
+      </Routes>
+    </ContextProvider>
   )
 }
 
