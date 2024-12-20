bed2604150b12b38f2641ee88f1f03ba8c0dcbac
JamshidPts <jamshidpts14@gmail.com>
2024-12-21 00:34:08 +0500

    I added main and section

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
+import Weather from '../components/weather/Weather'
 
 function MainPage() {
   return (
@@ -7,6 +8,11 @@ function MainPage() {
     <header>
         <Navbar />
     </header>
+    <main className='container'>
+      <section className=''>
+        <Weather />
+      </section>
+    </main>
     </>
   )
 }
